8dce13c19d2d4ea82672e940fd5945d563223ccb
Narazaka <info@narazaka.net>
2016-05-24 10:12:26 +0900

    class val

--- lib/system_command_helper.rb
+++ lib/system_command_helper.rb
@@ -3,29 +3,33 @@ require "system_command_helper/version"
 module SystemCommandHelper
   attr_accessor :no_exec, :quiet
 
+  @@no_exec = false
+  @@quiet = false
+  @@on_error = nil
+
   def sys(cmd)
-    unless @quiet
+    unless @@quiet
       puts "\e[35m#{Dir.pwd}$\e[0m #{cmd}"
     end
-    unless @no_exec
+    unless @@no_exec
       result = system cmd
-      if !result && @on_error
-        @on_error.call(cmd, result)
+      if !result && @@on_error
+        @@on_error.call(cmd, result)
       end
       result
     end
   end
 
   def cap(cmd)
-    unless @quiet
+    unless @@quiet
       puts "\e[35m#{Dir.pwd}$\e[0m `#{cmd}`"
     end
-    unless @no_exec
+    unless @@no_exec
       `#{cmd}`
     end
   end
 
   def on_error(&block)
-    @on_error = block
+    @@on_error = block
   end
 end
